6f0dae63add91921a2d5bec4999b659ab1e30d42
two_down <mister.franz02@gmail.com>
2020-06-07 15:56:44 +0700

    Добавьте файлы проекта.

---
+++ Dates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MM-DD-YYYY - America
+            // DD-MM-YYYY - Europe
+
+            var date = "";
+
+            using (var sr = new StreamReader("text.txt"))
+                date = sr.ReadToEnd();
+
+            var result = Convert(date);
+
+            Console.WriteLine("Converted:\n{0}\nto:\n{1}", date, result);
+        }
+
+        static string Convert(string date)
+        {
+            try
+            {
+                return Regex.Replace(date, @"\b(?<month>\d{1,2})-(?<day>\d{1,2})-(?<year>\d{2,4})\b",
+                                        "${day}-${month}-${year}", RegexOptions.None,
+                                         TimeSpan.FromMilliseconds(150));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return date;
+            }
+        }
+    }
+}
